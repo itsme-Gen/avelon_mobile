082e0eafac6855b67847111f172cc844207040ea
itsme-Gen <lirazangen@gmail.com>
2026-01-07 09:42:03 +0800

    Added forgot password UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,20 @@
 import Page from "../screens/pages/landingscreens";
 import SignUp from "../screens/auth/signup";
 import SignIn from "../screens/auth/signin";
+import ForgotPassword from "@/screens/forgotpassword/email";
+import OTP from "@/screens/forgotpassword/otp";
+import ResetPassword from "@/screens/forgotpassword/resetpassword";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import "../global.css";
+
 
 export type RootStackParamList = {
   Home: undefined;
   Signup: undefined;
   SignIn: undefined;
+  ForgotPassword: undefined;
+  OTP: undefined;
+  ResetPassword: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -18,6 +25,9 @@ export default function RootLayout() {
       <Stack.Screen name="Home" component={Page} options={{headerShown:false}} />
       <Stack.Screen name="Signup" component={SignUp} options={{headerShown:false}} />
       <Stack.Screen name="SignIn" component={SignIn} options={{headerShown:false}} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{headerShown:false}} />
+      <Stack.Screen name="OTP" component={OTP} options={{headerShown:false}} />
+      <Stack.Screen name="ResetPassword" component={ResetPassword} options={{headerShown:false}} />
     </Stack.Navigator>
   )
 }
--- screens/forgotpassword/resetpassword.tsx
+++ screens/forgotpassword/resetpassword.tsx
@@ -16,9 +16,8 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../app/_layout";
 import { useNavigation } from "@react-navigation/native";
 
-export default function SignIn() {
+export default function ResetPassword() {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-
   return (
     <SafeAreaView className="flex-1 bg-white" edges={['top']}>
       <View style={{ flex: 1 }}>
@@ -47,30 +46,13 @@ export default function SignIn() {
                   />
 
                   {/* TITLE */}
-                  <Text className="text-black text-3xl font-bold mb-10">
-                    Log In
+                  <Text className="text-black text-3xl font-semibold mb-10">
+                   Forgot Password
                   </Text>
                 </View>
 
-                {/* FORM */}
-                <View className="w-full mt-10">
-                  {/* EMAIL */}
-                  <View className="mb-4">
-                    <Text className="text-black font-bold text-lg ml-4 mb-2">
-                      Email
-                    </Text>
-                    <View className="flex-row items-center bg-[#ECECEC] rounded-full px-4 py-2">
-                      <Ionicons name="mail-outline" size={20} color="gray" />
-                      <TextInput
-                        className="ml-2 flex-1 py-3"
-                        placeholder="Enter your Email"
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-                      />
-                    </View>
-                  </View>
-
-                  {/* PASSWORD */}
+                <View className="mb-4">
+                     {/* PASSWORD */}
                   <View className="mb-4">
                     <Text className="text-black font-bold text-lg ml-4 mb-2">
                       Password
@@ -83,7 +65,7 @@ export default function SignIn() {
                       />
                       <TextInput
                         className="ml-2 flex-1 py-3"
-                        placeholder="Enter your Password"
+                        placeholder="Create a Password"
                         secureTextEntry
                       />
                       <Ionicons
@@ -94,36 +76,42 @@ export default function SignIn() {
                     </View>
                   </View>
 
-                  {/* FORGOT PASSWORD */}
-                  <View className="items-end mb-10">
-                    <TouchableOpacity
-                      onPress={() => navigation.navigate('ForgotPassword')}
-                    >
-                      <Text className="text-black font-semibold">Forgot Password?</Text>
-                    </TouchableOpacity>
+                  {/* CONFIRM PASSWORD */}
+                  <View className="mb-4">
+                    <Text className="text-black font-bold text-lg ml-4 mb-2">
+                      Confirm Password
+                    </Text>
+                    <View className="flex-row items-center bg-[#ECECEC] rounded-full px-4 py-2">
+                      <Ionicons
+                        name="lock-closed-outline"
+                        size={20}
+                        color="gray"
+                      />
+                      <TextInput
+                        className="ml-2 flex-1 py-3"
+                        placeholder="Re-enter your Password"
+                        secureTextEntry
+                      />
+                      <Ionicons
+                        name="eye-off-outline"
+                        size={20}
+                        color="gray"
+                      />
+                    </View>
+                    </View>
                   </View>
                 </View>
-              </View>
             </TouchableWithoutFeedback>
           </ScrollView>
         </KeyboardAvoidingView>
 
         {/* Fixed Bottom Section */}
         <View className="mb-10 px-5 py-4 bg-white border-t border-gray-100">
-          <View className="flex flex-row justify-center items-center mb-3">
-            <Text>Don't have an Account?</Text>
-            <TouchableOpacity
-              onPress={() => navigation.navigate('Signup')}
-            >
-              <Text className="text-black font-bold"> Sign Up</Text>
-            </TouchableOpacity>
-          </View>
-
           <TouchableOpacity
             className="bg-black w-full justify-center items-center py-4 rounded-full"
-            onPress={() => alert('Log In button pressed')}
+            onPress={() => alert('Confirm button pressed')}
           >
-            <Text className="text-white text-lg font-bold">Log In</Text>
+            <Text className="text-white text-lg font-bold">Confirm</Text>
           </TouchableOpacity>
         </View>
       </View>
