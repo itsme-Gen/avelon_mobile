1cb7560dd45d7cb2abbf12e46a30bffa5da72db7
Jiwuuuu <andr.amrinto@gmail.com>
2026-03-23 11:10:32 +0800

    fix: metro bundle

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,15 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require('path');
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
+
+// Allow Metro to resolve the local @avelon_capstone/types package
+const avelonTypesPath = path.resolve(__dirname, '../avelon_types');
+config.watchFolders = [avelonTypesPath];
+config.resolver.nodeModulesPaths = [
+    path.resolve(__dirname, 'node_modules'),
+    path.resolve(avelonTypesPath, 'node_modules'),
+];
 
 module.exports = withNativeWind(config, { input: './src/styles/global.css' })
